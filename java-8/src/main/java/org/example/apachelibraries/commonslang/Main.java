2ba00f4adc05a7aa03b4ecf60ef761cc615eb4bf
chuttmateo <chuttmateo0@gmail.com>
2023-11-27 09:23:08 -0300

    more methods from StringUtils

--- java-8/src/main/java/org/example/apachelibraries/commonslang/Main.java
+++ java-8/src/main/java/org/example/apachelibraries/commonslang/Main.java
@@ -2,6 +2,8 @@ package org.example.apachelibraries.commonslang;
 
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -20,10 +22,22 @@ public class Main {
         Replace a specific character in the string.
         Count the occurrences of a certain character in the string.*/
 
-        String text = "example test";
+        String text = "example text";
         System.out.println("isEmpty: " + StringUtils.isEmpty(text));
+        //text.isBlank(); // this will throw an exception if "text" is null
+        String[] str =  StringUtils.split(text, " ");
+        System.out.println("split: " + Arrays.toString(str));
+
+        System.out.println("join: " + StringUtils.join(str, "-"));
+
         System.out.println("Capitalize the first letter of this string: " + StringUtils.capitalize(text));
-        System.out.println("Replace a specific character: " + StringUtils.replace(text, "s", "x"));
-        System.out.println("Count occurrences: " + StringUtils.countMatches(text, "e")); //"example test"
+        System.out.println("Replace a specific character: " + StringUtils.replace(text, "x", "X"));
+        System.out.println("Count occurrences: " + StringUtils.countMatches(text, "e"));
+        System.out.println(StringUtils.trim(text));
+        System.out.println(StringUtils.strip(text));
+        System.out.println(StringUtils.right(text, 3));
+        System.out.println(StringUtils.left(text, 3));
+
+
     }
 }
